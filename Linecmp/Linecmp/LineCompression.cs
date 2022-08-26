507bdd1005d181b49ca2e363991ae11ba334de31
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-26 09:04:17 +0530

    [kavya] Add: Line compression

---
+++ Linecmp/Linecmp/LineCompression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linecmp
+{
+    public class LineCompression
+    {
+        int x1, x2, y1, y2;
+        public LineCompression(int _x1, int _x2, int _y1, int _y2)
+        {
+            this.x1 = _x1;
+            this.x2 = _x2;
+            this.y1 = _y1;
+            this.y2 = _y2;
+        }
+        public void CalculationLength()
+        {
+            double linelength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(linelength);
+        }
+    }
+
+}
---
+++ Linecmp/Linecmp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Linecmp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LineCompression comparision = new LineCompression(1, 2, 3, 4);
+            comparision.CalculationLength();
+
+        }
+    }
+}
